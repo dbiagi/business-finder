d093812bd770bf9d9fcb9750a972f5baa3e26638
Diego de Biagi <diegobiagiviana@gmail.com>
2017-03-15 08:05:40 -0300

    Paginação ok, só falta arrumar a busca e já era.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
 
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+
             new AppBundle\AppBundle(),
         ];
 
--- src/AppBundle/Controller/PublicController/IndexController.php
+++ src/AppBundle/Controller/PublicController/IndexController.php
@@ -20,25 +20,29 @@ class IndexController extends Controller {
      */
     public function indexAction(Request $request) {
         $criteria = $request->get(SearchType::NAME);
+        $page = $request->get('page', 1);
 
         // @TODO usar os criterios de busca para filtrar a listagem
 
         $businessRepo = $this->getDoctrine()->getRepository('AppBundle:Business');
         $businessCategoryRepo = $this->getDoctrine()->getRepository('AppBundle:BusinessCategory');
 
+        $businesses = $this->get('knp_paginator')
+            ->paginate($businessRepo->findByCriteria($criteria), $page, 10);
+
         return $this->render('pages/home.html.twig', [
-            'businesses' => $businessRepo->findAll(),
-            'categories' => $businessCategoryRepo->findAll()
+            'businesses' => $businesses,
+            'categories' => $businessCategoryRepo->findAll(),
         ]);
     }
 
-    public function searchAction(Request $request){
+    public function searchAction(Request $request) {
         $data = $request->get(SearchType::NAME);
 
         $form = $this->createForm(SearchType::class, $data);
 
         return $this->render('search.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(),
         ]);
     }
 }
