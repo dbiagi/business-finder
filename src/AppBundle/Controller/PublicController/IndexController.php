3c01ba591967885680b3dc03f64c1a3aeb3ef5ea
Diego de Biagi <diegobiagiviana@gmail.com>
2017-03-15 00:25:33 -0300

    Adicionado form de busca.

--- src/AppBundle/Controller/PublicController/IndexController.php
+++ src/AppBundle/Controller/PublicController/IndexController.php
@@ -2,8 +2,10 @@
 
 namespace AppBundle\Controller\PublicController;
 
+use AppBundle\Form\SearchType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class IndexController extends Controller {
@@ -13,15 +15,30 @@ class IndexController extends Controller {
      *
      * @Route("/", name="app_home")
      *
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @param Request $request
+     * @return Response
      */
-    public function indexAction() {
+    public function indexAction(Request $request) {
+        $criteria = $request->get(SearchType::NAME);
+
+        // @TODO usar os criterios de busca para filtrar a listagem
+
         $businessRepo = $this->getDoctrine()->getRepository('AppBundle:Business');
         $businessCategoryRepo = $this->getDoctrine()->getRepository('AppBundle:BusinessCategory');
 
         return $this->render('pages/home.html.twig', [
             'businesses' => $businessRepo->findAll(),
             'categories' => $businessCategoryRepo->findAll()
+        ]);
+    }
+
+    public function searchAction(Request $request){
+        $data = $request->get(SearchType::NAME);
+
+        $form = $this->createForm(SearchType::class, $data);
+
+        return $this->render('search.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
