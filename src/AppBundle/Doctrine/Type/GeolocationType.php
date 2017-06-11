6c7df14805d7aa71ff3521ab39e7e58f0140e07d
Diego de Biagi <diegobiagiviana@gmail.com>
2017-06-11 17:39:15 -0300

    Novo doctrine type: Geolocation

---
+++ src/AppBundle/Doctrine/Type/GeolocationType.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace AppBundle\Doctrine\Type;
+
+use AppBundle\Entity\Geolocation;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+
+class GeolocationType extends Type
+{
+    const GEOLOCATION = 'geolocation';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return self::GEOLOCATION;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return 'POINT';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        list($longitude, $latitude) = sscanf($value, 'POINT(%f %f)');
+
+        return new Geolocation($latitude, $longitude);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if ($value instanceof Geolocation) {
+            $value = sprintf('POINT(%F, %F)', $value->getLongitude(), $value->getLatitude());
+        }
+
+        return $value;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function canRequireSQLConversion()
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToPHPValueSQL($sqlExpr, $platform)
+    {
+        return sprintf('AsText(%s)', $sqlExpr);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function convertToDatabaseValueSQL($sqlExpr, AbstractPlatform $platform)
+    {
+        return sprintf('PointFromText(%s)', $sqlExpr);
+    }
+}
